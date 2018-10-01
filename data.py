1035190d2ebd2591a87ef8fe030b38a65a65fab8
Zac Holland <zacharyholland@gmail.com>
2018-10-01 13:43:50 -0600

    Added comments for data.py

--- data.py
+++ data.py
@@ -3,14 +3,20 @@ from os import listdir
 class Data:
   GALAXIES_DIR_REL_PATH = "data/Rotmod_LTG/"
 
+  # Goes into the specified folder for Galaxies and returns
+  #  all files in the folder. Defaults to the class const, but
+  #  can be provided for testing.
   def getGalaxyFiles(self, galaxiesDirRelPath=GALAXIES_DIR_REL_PATH):
     return listdir(galaxiesDirRelPath)
 
+  # Given a line of data from a file, convert each string value
+  #  (which are sepperated by tabs) to floats
   def dataLineToValues(self, line):
     stringValues = line.split("\t")
     floatValues = list(map(lambda x: float(x), stringValues))
     return floatValues
 
+  # Returns all the galaxy data in the given folder
   def getGalaxyData(self, galaxiesDirRelPath=GALAXIES_DIR_REL_PATH):
     files = self.getGalaxyFiles(galaxiesDirRelPath)
 
