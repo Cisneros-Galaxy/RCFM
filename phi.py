4a906509a5261785f1c5e3f63b72d37d6c0832b2
Zac Holland <zacharyholland@gmail.com>
2019-01-24 20:19:37 -0700

    phi now produces correct data with small hiccup, looking into that

--- phi.py
+++ phi.py
@@ -1,13 +1,13 @@
 from scipy.integrate import quad as integrate
 
+# Variables needed for other functions
+c = 3 * (10**5) # km/s
+
 # This is the function for calc Phi.
 # NOTE: This function is to be used with Scypi.integrate.quad - when doing this you can specify
 #   arguments to pass in, but it will always pass in the arguments AFTER x (or r in our case)
-def phiFunc(r, v):
-    return (v)/(r*(c*c))
-
-# Variables needed for other functions
-c = 3 * (10**5) # km/s
+def phiFunc(r, vSquared):
+    return (vSquared)/(r*(c*c))
 
 # CalcPhi - Given radii and vSquared, this will return Phi at each radius
 # Params
@@ -15,11 +15,13 @@ c = 3 * (10**5) # km/s
 #  vSquared - an array of vSquared at each radius
 def CalcWithRadiiAndVSquared(radii, vSquared):
   phi = []
+  phi.append(0)
   for i in range(len(radii)):
+    if i == 0:
+        continue
+
     # Get min radius. Will be 0 for the first time, then the previous r for every other
-    rMin = 0
-    if i != 0:
-        rMin = radii[i-1]
+    rMin = radii[i-1]
         
     # Max radius will always be the radius at current index
     rMax = radii[i]
@@ -27,15 +29,16 @@ def CalcWithRadiiAndVSquared(radii, vSquared):
 
     # New Phi data points are cumulative, so we need to add 
     #   the previous point (if it exists)
-    newPhiDataPoint = 0
-    if i != 0:
-        newPhiDataPoint += phi[i-1]
+    newPhiDataPoint = phi[i-1]
     
     # Calculate the integral at this point
-    integral, err = integrate(phiFunc, rMin, rMax, (vS))
+    integral, err = integrate(phiFunc, rMin, rMax, vS)
+
     # Add it to the newPoint (essentially adding to the sum)
     newPhiDataPoint += integral
+
     # Append the new point to the array!
     phi.append(newPhiDataPoint)
     
+  phi = phi[1:]
   return phi
